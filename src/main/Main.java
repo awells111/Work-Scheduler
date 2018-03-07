90085efadc169c06a2d020823afc45bfc96f8138
awells111 <awells111@gmail.com>
2018-03-06 21:20:36 -0500

    Translations working

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,14 @@ package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import main.view_controller.LoginController;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 import static main.view_controller.LoginController.FXML_LOGIN;
 
@@ -21,7 +22,7 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         this.primaryStage = primaryStage;
         getPrimaryStage().setTitle("Work Scheduler");
 
@@ -31,14 +32,19 @@ public class Main extends Application {
 
     private void initRootLayout() {
         try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource(FXML_LOGIN));
+            //todo Required for A. Log-in Form **Requires import java.util.Locale;
+            //Locale.setDefault(new Locale("fr"));
+            //Locale.setDefault(new Locale("es"));
+
+            ResourceBundle bundle = ResourceBundle.getBundle("main.rb");
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource(FXML_LOGIN), bundle);
             AnchorPane rootLayout = loader.load();
             Scene scene = new Scene(rootLayout);
             getPrimaryStage().setScene(scene);
             getPrimaryStage().show();
             LoginController controller = loader.getController();
             controller.setMainApp(this);
+
         } catch (IOException var5) {
             var5.printStackTrace();
         }
